43aa4f7b863307f925f78bec49f5239c36c63f8f
Juandagarc <juandagarce@gmail.com>
2024-04-08 17:00:25 -0500

    detecta tipo S y hace el split

--- src/main.cpp
+++ src/main.cpp
@@ -1,62 +1,95 @@
 #include <iostream>
 #include <fstream>
-#include "parser.hh"
-#include "encoder.hh" // Incluir el encabezado encoder.hh
+#include <vector>
+//#include "parser.hh"
+//#include "encoder.hh" // Incluir el encabezado encoder.hh
+
+using namespace std;
+
+bool hasComment(string& l) {
+    return l.find("#") != l.npos;
+    }
+
+ void removeComment(string& l) {
+    if (hasComment(l)){
+        l.erase(l.find("#"));
+    }
+ }
+
+bool isTypeS(string& l) {
+    if(l.find("sw") != l.npos){
+        return 1;
+    } else if(l.find("sb") != l.npos){
+        return 1;
+    } else if(l.find("sh") != l.npos){
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+string extractInst(string& l){
+    if(l.find("sw") != l.npos){
+        l.erase(l.find("sw"), 2);
+        return "sw";
+    } else if(l.find("sb") != l.npos){
+        l.erase(l.find("sb"), 2);
+        return "sb";
+    } else if(l.find("sh") != l.npos){
+        l.erase(l.find("sh"), 2);
+        return "sh";
+    }
+    return "";
+}
+
+vector<string> split(string& l){
+    vector<string> result;
+    while(l.find(',') != l.npos){
+        size_t pos = l.find(',');
+        result.push_back(l.substr(0, pos));
+        l.erase(0, pos + 1); // Avanzar más allá del carácter de coma
+    }
+    result.push_back(l.substr(0, l.length()));
+    //print the vector
+    for (const auto& element : result) {
+        cout << element << " ";
+    }
+    cout << endl;
+
+    return result;
+}
 
 int main() {
-    std::ifstream inputFile("../test/input.asm");
+    ifstream inputFile("../test/input.asm");
     if (!inputFile.is_open()) {
-        std::cerr << "Error al abrir el archivo de entrada." << std::endl;
+        cerr << "Error al abrir el archivo de entrada." << endl;
         return 1;
     }
 
-    std::ofstream outputFile("../test/output.txt");
+    ofstream outputFile("../test/output.txt");
     if (!outputFile.is_open()) {
-        std::cerr << "Error al abrir el archivo de salida." << std::endl;
+        cerr << "Error al abrir el archivo de salida." << endl;
         return 1;
     }
 
-    Parser parser;
 
     // Procesar cada línea del archivo de entrada
-    std::string line;
-    while (std::getline(inputFile, line)) {
-        // Si es una instrucción, parsearla y escribir sus componentes en el archivo de salida
-        if (parser.identifyLineType(line) == LineType::Instruction) {
-            InstructionComponents components = parser.parseInstruction(line);
-            InstructionType type = Encoder::getInstructionType(components.opcode);
-
-            switch (type) {
-                case InstructionType::R_Type:
-                    // Aquí podrías construir el RTypeInstruction y asignarlo a components.rTypeInstruction
-                    break;
-                case InstructionType::I_Type:
-                    // Construye y asigna el ITypeInstruction
-                    break;
-                case InstructionType::S_Type:
-                    // Construye y asigna el STypeInstruction
-                    break;
-                case InstructionType::B_Type:
-                    // Construye y asigna el BTypeInstruction
-                    break;
-                case InstructionType::U_Type:
-                    // Construye y asigna el UTypeInstruction
-                    break;
-                case InstructionType::J_Type:
-                    // Construye y asigna el JTypeInstruction
-                    break;
-                default:
-                    std::cerr << "Tipo de instrucción desconocido." << std::endl;
-                    continue;
-            }
-            uint32_t encodedInstruction = 0; 
-            outputFile << encodedInstruction << std::endl;
+    string line;
+    while (getline(inputFile, line)) {
+        if (hasComment(line)) {
+            removeComment(line);
+            cout << line << endl;
+        }
+        if (isTypeS(line)){
+            cout << "Tipo S" << endl;
+            cout << extractInst(line) << " extraido." << endl;
+            split(line);
         }
     }
 
     inputFile.close();
     outputFile.close();
 
-    std::cout << "Proceso completado. Se ha generado el archivo output.txt." << std::endl;
+    cout << "Proceso completado. Se ha generado el archivo output.txt." << endl;
     return 0;
 }
