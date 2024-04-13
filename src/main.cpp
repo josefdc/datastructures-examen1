1cbb8872963beb4b0975a17f105979b24a754d63
Juandagarc <juandagarce@gmail.com>
2024-04-13 11:35:01 -0500

    type s,r,i

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-//#include "parser.hh"
-//#include "encoder.hh" // Incluir el encabezado encoder.hh
+#include <bitset>
+#include <string>
 
 using namespace std;
 
@@ -16,30 +16,88 @@ bool hasComment(string& l) {
     }
  }
 
+string encodeRegister(string n) {
+    int arg = stoi(n.substr(2,n.length()));
+    bitset<5> binary(arg);
+    return binary.to_string();
+}
+
+string encodeRs1 (string n) {
+    int arg = stoi(n.substr(1,n.length()));
+    bitset<5> binary(arg);
+    return binary.to_string();
+}
+
+string encodeConstNumber(string n ) {
+    int arg = stoi(n.substr(0,n.length()));
+    bitset<12> binary(arg);
+    return binary.to_string();
+}
+
+vector<string> separeConstNumber( string n ) {
+    vector<string> result;
+    result.push_back(n.substr(0, 7));
+    result.push_back(n.substr(7, n.length()));
+    return result;
+}
+
+
+
+// beq x0, x0, swap
 bool isTypeS(string& l) {
-    if(l.find("sw") != l.npos){
-        return 1;
-    } else if(l.find("sb") != l.npos){
-        return 1;
-    } else if(l.find("sh") != l.npos){
-        return 1;
-    } else {
-        return 0;
-    }
+    if(l.find("sw") != l.npos) return 1;
+    if(l.find("sb") != l.npos) return 1;
+    if(l.find("sh") != l.npos) return 1;
+    return 0;
+}
+
+bool isTypeR(string& l) {
+    if(l.find("addi") != l.npos) return 0;
+    if(l.find("add") != l.npos) return 1;
+    if(l.find("sub") != l.npos) return 1;
+    if(l.find("xor") != l.npos) return 1;
+    return 0;
+}
+
+bool isTypeI(string& l) {
+    if(l.find("addi") != l.npos) return 1;
+    if(l.find("lw") != l.npos) return 1;
+    return 0;
 }
 
 string extractInst(string& l){
-    if(l.find("sw") != l.npos){
-        l.erase(l.find("sw"), 2);
-        return "sw";
-    } else if(l.find("sb") != l.npos){
-        l.erase(l.find("sb"), 2);
-        return "sb";
-    } else if(l.find("sh") != l.npos){
-        l.erase(l.find("sh"), 2);
-        return "sh";
-    }
+    if(l.find("sw") != l.npos){ l.erase(l.find("sw"), 2);return "sw";}
+    if(l.find("sb") != l.npos){ l.erase(l.find("sb"), 2);return "sb";}
+    if(l.find("sh") != l.npos){ l.erase(l.find("sh"), 2);return "sh";}
+    if(l.find("addi") != l.npos){ l.erase(l.find("addi"), 4);return "addi";}
+    if(l.find("lw") != l.npos){ l.erase(l.find("lw"), 2);return "lw";}
+    if(l.find("add") != l.npos){ l.erase(l.find("add"), 3);return "add";}
+    if(l.find("sub") != l.npos){ l.erase(l.find("sub"), 3);return "sub";}
+    if(l.find("xor") != l.npos){ l.erase(l.find("xor"), 3);return "xor";}
     return "";
+}
+
+string extractFunc3(string& inst){
+    if( inst=="sw" )return "010";
+    if( inst=="sh" )return "001";
+    if( inst=="sb" )return "000";
+    return "";
+}
+
+vector<string> separeImn(string l){
+    vector<string> result;
+    while(l.find('(') != l.npos){
+        size_t pos = l.find('(');
+        result.push_back(l.substr(0, pos));
+        l.erase(0, pos + 1);
+    }
+    while(l.find(')') != l.npos){
+        size_t pos = l.find(')');
+        result.push_back(l.substr(0, pos));
+        l.erase(0, pos + 1);
+    }
+
+    return result;
 }
 
 vector<string> split(string& l){
@@ -50,13 +108,67 @@ vector<string> split(string& l){
         l.erase(0, pos + 1); // Avanzar más allá del carácter de coma
     }
     result.push_back(l.substr(0, l.length()));
-    //print the vector
-    for (const auto& element : result) {
-        cout << element << " ";
-    }
-    cout << endl;
 
     return result;
+}
+
+string processTypeS(string inst, string rest) {
+    cout << "processTypeS" << endl;
+    vector<string> args = split(rest);
+    string opcode = "0100011";
+    string rs2 = encodeRegister(args[0]);
+    string func3 = extractFunc3(inst);
+    string dest = args[1];
+    vector<string> imn = separeImn(dest);
+    //las constantes
+    vector<string> numberConst= separeConstNumber(encodeConstNumber(imn[0]));
+    string rs1= encodeRs1(imn[1]);
+    // cout << "opcode " << opcode << endl;
+    // cout << "rs2 " << rs2 << endl;
+    // cout << "func3 " << func3 << endl;
+    // cout << "rs1 " << rs1 << endl;
+    string result = numberConst[0] + rs2 + rs1 + func3 + numberConst[1] + opcode;
+    return result;
+}
+
+string processTypeR(string inst, string rest) {
+    cout << "processTypeR" << endl;
+    string func7;
+    string func3;
+    if(inst == "add"){ func7 = "0000000"; func3 = "000"; }
+    if(inst == "sub"){ func7 = "0100000"; func3 = "000"; }
+    if(inst == "xor"){ func7 = "0000000"; func3 = "100"; }
+    vector<string> args = split(rest);
+    string rs2 = encodeRegister(args[2]);
+    string rs1 = encodeRegister(args[1]);
+    string rd = encodeRegister(args[0]);
+    string opcode = "0110011";
+    // cout << "func7: " << func7 << endl;
+    // cout << "rs2: " << rs2 << endl;
+    // cout << "rs1: " << rs1 << endl;
+    // cout << "func3: " << func3 << endl;
+    // cout << "rd: " << rd << endl;
+    string result = func7 + rs2 + rs1 + func3 +rd + opcode;
+    return result;
+    }
+
+string processTypeI(string inst, string rest) {
+    cout << "processTypeI" << endl;
+    vector<string> args = split(rest);
+    if (inst == "addi")
+    {
+        string imn = encodeConstNumber(args[2]);
+        string rs1 = encodeRegister(args[1]);
+        string func3 = "000";
+        string rd = encodeRegister(args[0]);
+        string opcode = "0010011";
+        // cout << "imn: " << imn << endl;
+        // cout << "rs1: " << rs1 << endl;
+        // cout << "rd: " << rd << endl;
+        string result = imn + rs1 + func3 + rd + opcode;
+        return result;
+    }
+    return "";
 }
 
 int main() {
@@ -72,18 +184,24 @@ int main() {
         return 1;
     }
 
-
     // Procesar cada línea del archivo de entrada
     string line;
     while (getline(inputFile, line)) {
         if (hasComment(line)) {
             removeComment(line);
-            cout << line << endl;
         }
-        if (isTypeS(line)){
-            cout << "Tipo S" << endl;
-            cout << extractInst(line) << " extraido." << endl;
-            split(line);
+        string inst = extractInst(line);
+        if(isTypeS(inst)) {
+            string typeS = processTypeS(inst, line);
+            cout << typeS << endl;
+        }
+        if(isTypeR(inst)) {
+            string typeR = processTypeR(inst, line);
+            cout << typeR << endl;
+        }
+        if(isTypeI(inst)){
+            string typeI = processTypeI(inst, line);
+            cout << typeI << endl;
         }
     }
 
--- test/input.asm
+++ test/input.asm
@@ -1,19 +1,28 @@
 # Ejemplo de instrucciones tipo R: add, sub, xor...
-add x1, x2, x3
-sub x4, x5, x6
-xor x7, x8, x9
+add x1, x2, x3     # x1 = x2 + x3
+sub x4, x5, x6     # x4 = x5 - x6
+xor x7, x8, x9     # x7 = x8 ^ x9
+
+# Ejemplo de instrucciones tipo I: addi, lw...
+addi x10, x11, 10  # x10 = x11 + 10
+lw x12, 100(x13)   # Carga el contenido de la memoria en la dirección x13 + 100 en x12
+
 # Ejemplo de instrucciones tipo S: sw, sh, sb...
-sw x10, 20(x11)    # Guarda el contenido de x10 en la dirección de memoria calculada por x11 + 20
-sh x12, -4(x13)    # Guarda los 16 bits menos significativos de x12 en la dirección de memoria calculada por x13 - 4
-sb x14, 100(x15)   # Guarda el byte menos significativo de x14 en la dirección de memoria calculada por x15 + 100
+sw x14, 20(x15)    # Guarda el contenido de x14 en la dirección de memoria calculada por x15 + 20
+sh x16, -4(x17)    # Guarda los 16 bits menos significativos de x16 en la dirección de memoria calculada por x17 - 4
+sb x18, 100(x19)   # Guarda el byte menos significativo de x18 en la dirección de memoria calculada por x19 + 100
+
 # Ejemplo de instrucciones tipo B: beq, bne, blt...
-beq x16, x17, 50   # Salta a la etiqueta 50 si el contenido de x16 es igual al contenido de x17
-bne x18, x19, 100  # Sa|lta a la etiqueta 100 si el contenido de x18 no es igual al contenido de x19
-blt x20, x21, 200  # Salta a la etiqueta 200 si el contenido de x20 es menor que el contenido de x21
+beq x20, x21, 50   # Salta a la etiqueta 50 si el contenido de x20 es igual al contenido de x21
+bne x22, x23, 100  # Salta a la etiqueta 100 si el contenido de x22 no es igual al contenido de x23
+blt x24, x25, 200  # Salta a la etiqueta 200 si el contenido de x24 es menor que el contenido de x25
+
 # Ejemplo de instrucciones tipo U: lui, auipc...
-lui x22, 1000      # Carga la constante 1000 en los bits más significativos de x22
-auipc x23, 200     # Carga la dirección actual más la constante 200 en los bits más significativos de x23
+lui x26, 1000      # Carga la constante 1000 en los bits más significativos de x26
+auipc x27, 200     # Carga la dirección actual más la constante 200 en los bits más significativos de x27
+
 # Ejemplo de instrucción tipo J: jal
-jal x24, 500       # Salta a la etiqueta 500 y guarda la dirección de retorno en x24
+jal x28, 500       # Salta a la etiqueta 500 y guarda la dirección de retorno en x28
+
 # Ejemplo de instrucción nop
 nop                # No realiza ninguna operación
